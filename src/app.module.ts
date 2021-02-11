d3005e7e62385edba91cbcc3b059801862c5d3e2
victor <1045606768@qq.com>
2021-02-11 17:45:17 +0800

    remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,7 @@ import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/c
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
-import { EventsModule } from './events/events.module';
 import { PostModule } from './post/post.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { WowTokenModule } from './wow-token/wow-token.module';
@@ -13,8 +11,6 @@ import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
-    CatsModule,
-    EventsModule,
     // 这里的 { useCreateIndex: true } 是为了解决
     // https://mongoosejs.com/docs/deprecations.html#ensure Index()的报错
     MongooseModule.forRoot('mongodb://manager:main_db_manager@89.208.248.23:27017/main', { useCreateIndex: true }),
