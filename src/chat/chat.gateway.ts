d5ece30a709dcf0a34bdeea5b1c906409124481e
victor <1045606768@qq.com>
2021-02-11 19:59:36 +0800

    处理多个聊天室客户端列表返回全部客户端的问题

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -12,29 +12,31 @@ export class ChatGateway {
   defaultRoom: string;
 
   handleConnection(client: Socket) {
-    console.log('connected: ', client.id);
-    client.join(this.defaultRoom);
-    const { userName, userId, avatar = '' } = client.handshake.query;
-    this.server.to(this.defaultRoom).emit('join', {
+    const { userName, userId, avatar = '', room = this.defaultRoom } = client.handshake.query;
+    client.join(room);
+    this.server.in(room).emit('join', {
       id: client.id,
       userInfo: { userId, userName, avatar },
     });
+    console.log('connected: ', `${client.id} has joind room ${room}`);
   }
 
   handleDisconnect(client: any) {
-    console.log('disconnected: ', client.id);
-    const { userName, userId, avatar = '' } = client.handshake.query;
-    this.server.to(this.defaultRoom).emit('leave', 'leave', {
+    const { userName, userId, avatar = '', room = this.defaultRoom } = client.handshake.query;
+    this.server.to(room).emit('leave', 'leave', {
       id: client.id,
       userInfo: { userId, userName, avatar },
     });
+    console.log('disconnected: ', `${client.id} has leaved room ${room}`);
   }
 
   @SubscribeMessage('online-list')
-  async getOnlineList(): Promise<any> {
-    const { connected } = this.server.in(this.defaultRoom);
-    const res = [...Object.keys(connected)].map(key => {
-      const connection = connected[key];
+  async getOnlineList(@ConnectedSocket() client: Socket): Promise<any> {
+    const { room } = client.handshake.query;
+    // https://stackoverflow.com/a/25028953
+    const clientIds = this.server.sockets.adapter.rooms[`${room}`].sockets;
+    const res = [...Object.keys(clientIds)].map(key => {
+      const connection = this.server.sockets.connected[key];
       const { userId, userName, avatar = '' } = connection.handshake.query;
       return {
         userInfo: {
@@ -60,6 +62,7 @@ export class ChatGateway {
     // to: 'public',
     // message,
     // timestamp: +new Date(),
-    this.server.to('public').emit('message', data);
+    const { room = this.defaultRoom } = client.handshake.query;
+    this.server.to(room).emit('message', data);
   }
 }
