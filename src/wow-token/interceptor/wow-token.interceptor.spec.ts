903f8af4f370b04c819b424960ac59a6f2703a7d
victor <1045606768@qq.com>
2021-02-11 17:45:31 +0800

    add interceptor to wow-token

---
+++ src/wow-token/interceptor/wow-token.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { WowTokenInterceptor } from './wow-token.interceptor';
+
+describe('WowTokenInterceptor', () => {
+  it('should be defined', () => {
+    expect(new WowTokenInterceptor()).toBeDefined();
+  });
+});
--- src/wow-token/wow-token.controller.ts
+++ src/wow-token/wow-token.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { WowToken } from './schemas/wow-token.schema';
 import { WowTokenService } from './wow-token.service';
+import { WowTokenInterceptor } from './interceptor/wow-token.interceptor';
 
 @Controller('wow-token')
 export class WowTokenController {
   constructor(private wowTokenService: WowTokenService) {}
 
+  @UseInterceptors(WowTokenInterceptor)
   @Get('list')
   async findAll(): Promise<WowToken[]> {
-    console.log('list');
     return this.wowTokenService.findAll();
   }
 }
