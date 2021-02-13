80eb9861ad4c9634b8ebc96d534fedefcbac95fc
victor <1045606768@qq.com>
2021-02-13 22:28:24 +0800

    add namespace

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,7 +1,7 @@
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({ namespace: '' })
 export class ChatGateway {
   constructor() {
     this.defaultRoom = 'public';
