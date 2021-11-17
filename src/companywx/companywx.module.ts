6d626230e44da9b0272f86ac4aa3e19e2942c416
lilei <1045606768@qq.com>
2021-11-17 10:26:55 +0800

    httpService添加interceptor, 实现失败重新请求

--- src/companywx/companywx.module.ts
+++ src/companywx/companywx.module.ts
@@ -1,4 +1,4 @@
-import { Module, HttpModule } from '@nestjs/common';
+import { Module, HttpModule, OnModuleInit, HttpService } from '@nestjs/common';
 import { CompanywxService } from './companywx.service';
 import { CompanywxController } from './companywx.controller';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -9,4 +9,44 @@ import { CompanyWx, CompanyWxSchema } from './schemas/companywx.schema';
   providers: [CompanywxService],
   imports: [HttpModule, MongooseModule.forFeature([{ name: CompanyWx.name, schema: CompanyWxSchema }])],
 })
-export class CompanywxModule {}
+// https://www.jianshu.com/p/ba1ca0bc87b5
+// https://juejin.cn/post/6968487137670856711
+export class CompanywxModule implements OnModuleInit {
+  constructor(private httpService: HttpService) {}
+  public onModuleInit() {
+    this.httpService.axiosRef.interceptors.response.use(
+      response => response,
+      err => {
+        const config = err.config;
+        if (!config) return Promise.reject(err);
+        // retry 具体接口配置的重发次数
+        const retry = 5;
+        // 设置用于记录重试计数的变量 默认为0
+        config.__retryCount = config.__retryCount || 0;
+
+        // 判断是否超过了重试次数
+        if (config.__retryCount >= retry) {
+          return Promise.reject(err);
+        }
+        // 重试次数
+        config.__retryCount += 1;
+
+        // 延时处理
+        const backoff = new Promise(function (resolve) {
+          setTimeout(function () {
+            resolve(0);
+          }, config.retryDelay || 500);
+        });
+        // 重新发起axios请求
+        return backoff.then(function () {
+          // 判断是否是JSON字符串
+          // TODO: 未确认config.data再重发时变为字符串的原因
+          if (config.data) {
+            config.data = JSON.parse(config.data);
+          }
+          return this.httpService(config);
+        });
+      },
+    );
+  }
+}
--- src/lib-reservation/lib-reservation.module.ts
+++ src/lib-reservation/lib-reservation.module.ts
@@ -1,4 +1,4 @@
-import { HttpModule, Module } from '@nestjs/common';
+import { HttpModule, HttpService, Module, OnModuleInit } from '@nestjs/common';
 import { LibReservationService } from './lib-reservation.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LibReservation, LibReservationSchema } from './schemas/lib-reservation.schema';
@@ -7,4 +7,44 @@ import { LibReservation, LibReservationSchema } from './schemas/lib-reservation.
   imports: [HttpModule, MongooseModule.forFeature([{ name: LibReservation.name, schema: LibReservationSchema }])],
   providers: [LibReservationService],
 })
-export class LibReservationModule {}
+// https://www.jianshu.com/p/ba1ca0bc87b5
+// https://juejin.cn/post/6968487137670856711
+export class LibReservationModule implements OnModuleInit {
+  constructor(private httpService: HttpService) {}
+  public onModuleInit() {
+    this.httpService.axiosRef.interceptors.response.use(
+      response => response,
+      err => {
+        const config = err.config;
+        if (!config) return Promise.reject(err);
+        // retry 具体接口配置的重发次数
+        const retry = 5;
+        // 设置用于记录重试计数的变量 默认为0
+        config.__retryCount = config.__retryCount || 0;
+
+        // 判断是否超过了重试次数
+        if (config.__retryCount >= retry) {
+          return Promise.reject(err);
+        }
+        // 重试次数
+        config.__retryCount += 1;
+
+        // 延时处理
+        const backoff = new Promise(function (resolve) {
+          setTimeout(function () {
+            resolve(0);
+          }, config.retryDelay || 500);
+        });
+        // 重新发起axios请求
+        return backoff.then(function () {
+          // 判断是否是JSON字符串
+          // TODO: 未确认config.data再重发时变为字符串的原因
+          if (config.data) {
+            config.data = JSON.parse(config.data);
+          }
+          return this.httpService(config);
+        });
+      },
+    );
+  }
+}
